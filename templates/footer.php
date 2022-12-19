be8a97c8fd04d61f1571b51caf0db4a44be424b6
CozminM <cozmin.mlesnita@gmail.com>
2022-12-19 15:03:19 +0200

    feat: Add search functionality to API calls

---
+++ templates/footer.php
@@ -0,0 +1,25 @@
+<!-- Footer -->
+<footer class="bg-dark text-center text-lg-start static-bottom">
+    <div class="container text-light p-4 pb-0">
+        <form action="">
+            <div class="row">
+                <div class="col-auto mb-4 mb-md-0">
+                    <p class="pt-2">
+                        <strong>Sign up for our newsletter</strong>
+                    </p>
+                </div>
+                <div class="col-md-5 col-12 mb-4 mb-md-0">
+                    <div class="form-outline mb-4">
+                        <input type="email" id="form5Example25" class="form-control" />
+                        <label class="form-label" for="form5Example25">Email address</label>
+                    </div>
+                </div>
+                <div class="col-auto mb-4 mb-md-0">
+                    <button type="submit" class="btn btn-primary mb-4">
+                        Subscribe
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</footer>
---
+++ templates/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous" defer></script>
+    <title>Revista DAW - Index</title>
+</head>
+
+<body id="page-top">
+    <!-- Navbar -->
+    <nav class="navbar navbar-dark navbar-expand-lg bg-dark static-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Revista</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search" method="get">
+                    <input class="form-control me-2" name="search" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
